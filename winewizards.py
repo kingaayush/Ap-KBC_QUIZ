0d8f452bd5a2a7df0040bd9b35e15c6d340bdd07
kingaayush <134366305+kingaayush@users.noreply.github.com>
2023-09-20 03:44:58 +0530

    Update and rename KBCQUIZ.CPP to winewizards.py

---
+++ winewizards.py
@@ -0,0 +1,109 @@
+name=input("Please enter your name:\n")
+a=int(input('''In which subject do you face difficulty:- \nPress 1 for Maths \nPress 2 for Physics\nPress 3 for chemistry\n'''))
+if a==1:
+    c=int(input('''Enter the topics in which you are weak:- \nPress 1 for calculus \nPress 2 for vector and 3D \nPress 3 forprobability\n'''))
+    if c==1:
+        k=int(input('''Enter the manner of learning in which you are comfortable:- \nPress 1 for visualisation \nPress 2 for practical \nPress 3 for theoretical \nPress 4 for discussion based '''))
+        if k==1:
+            print("As per our analysis, Prof. Subramaniam & Prof. Sita are recommended for you.")
+        elif k==2:
+            print("As per our analysis, Prof. X & Prof. Alakh are recommended for you.")
+        elif k==3:
+            print("As per our analysis, Prof. Y & Prof. Nandini are recommended for you.")
+        elif k==4:
+            print("As per our analysis, Prof. Z & Prof. Dhanush are recommended for you.")  
+    if c==2:
+        k=int(input('''Enter the manner of learning in which you are comfortable:- \nPress 1 for Visualisation \nPress 2 for Practical \nPress 3 for Theoretical \nPress 4 for discussion based '''))
+        if k==1:
+            print("As per our analysis, Prof. Anish & Prof. Gauri are recommended for you.")
+        elif k==2:
+            print("As per our analysis, Prof. Raman & Prof. Alakh are recommended for you.")
+        elif k==3:
+            print("As per our analysis, Prof. Salman & Prof. Swamy are recommended for you.")
+        elif k==4:
+            print("As per our analysis, Prof. Isha & Prof. Dhanush are recommended for you.")
+    if c==3:
+        k=int(input('''Enter the manner of learning in which you are comfortable:- \nPress 1 for visualisation \nPress 2 for practical \nPress 3 for theoretical \nPress 4 for discussion based '''))
+        if k==1:
+            print("As per our analysis, Prof. Iqbal & Prof. Aayush are recommended for you.")
+        elif k==2:
+            print("As per our analysis, Prof. Parthiv & Prof. Teja are recommended for you.")
+        elif k==3:
+            print("As per our analysis, Prof. Vishnu & Prof. Kirti are recommended for you.")
+        elif k==4:
+            print("As per our analysis, Prof. Majnu & Prof. Disha are recommended for you.")
+elif a==2:
+    d=int(input('''Enter the topics in which you are weak:- \nPress 1 for optics \nPress 2 for modern physics \nPress 3 for mechanics \nPress 4 for electric current'''))
+    if d==1:
+        l=int(input('''Enter the manner of learning in which you are comfortable:- \nPress 1 for visualisation, press 2 for practical, press 3 for theoretical, press 4 for discussion based '''))
+        if l==1:
+            print("As per our analysis, Prof. Anish & Prof. Gauri are recommended for you.")
+        elif l==2:
+            print("As per our analysis, Prof. Alakh & Prof. Raman are recommended for you.")
+        elif l==3:
+            print("As per our analysis, Prof. Alakh & Prof. Raman are recommended for you.")
+        elif l==4:
+            print("As per our analysis, Prof. Sonam & Prof. Ankur are recommended for you.")
+    if d==2:
+        l=int(input('''Enter the manner of learning in which you are comfortable:- \nPress 1 for visualisation \nPress 2 for practical \nPress 3 for theoretical \nPress 4 for discussion based '''))
+        if l==1:
+            print("As per our analysis, Prof. aman & Prof. Gauri are recommended for you.")
+        elif l==2:
+            print("As per our analysis, Prof. Alakh & Prof. Raman are recommended for you.")
+        elif l==3:
+            print("As per our analysis, Prof. Alakh & Prof. Ram are recommended for you.")
+        elif l==4:
+            print("As per our analysis, Prof. Alakh & Prof. Arihant are recommended for you.")
+    if d==3:
+        l=int(input('''Enter the manner of learning in which you are comfortable:- \nPress 1 for visualisation \nPress 2 for practical \nPress 3 for theoretical \nPress 4 for discussion based '''))
+        if l==1:
+            print("As per our analysis, Prof. avinash & Prof. irfaan are recommended for you.")
+        elif l==2:
+            print("As per our analysis, Prof. elvish & Prof. sehnaaz are recommended for you.")
+        elif l==3:
+            print("As per our analysis, Prof. Abhishek & Prof. jiya are recommended for you.")
+        elif l==4:
+            print("As per our analysis, Prof. bebika & Prof. pooja are recommended for you.")
+    if d==4:
+        l=int(input('''Enter the manner of learning in which you are comfortable:- \nPress 1 for visualisation \nPress 2 for practical \nPress 3 for theoretical \nPress 4 for discussion based '''))
+        if l==1:
+            print("As per our analysis, Prof. cyrus & Prof. akhilesh are recommended for you.")
+        elif l==2:
+            print("As per our analysis, Prof. Roneel & Prof. savita are recommended for you.")
+        elif l==3:
+            print("As per our analysis, Prof. shailesh & Prof. shankar are recommended for you.")
+        elif l==4:
+            print("As per our analysis, Prof. nikita & Prof. nibhur are recommended for you.")                             
+elif a==3:
+    b=input("Enter sub topics in which you are weak :- Press 1 for organic \nPress 2 for inorganic \nPress 3 for physical\n")
+    if b=="1":
+        k=int(input('''Enter the manner of learning in which you are comfortable:- \nPress 1 for visualisation \nPress 2 for practical \nPress 3 for theoretical \nPress 4 for discussion based '''))
+        if k==1:
+            print("As per our analysis, Prof. Anish & Prof. Gauri are recommended for you.")
+        elif k==2:
+            print("As per our analysis, Prof. Alakh & Prof. Raman are recommended for you.")
+        elif k==3:
+            print("As per our analysis, Prof. Alakh & Prof. Raman are recommended for you.")
+        elif k==4:
+            print("As per our analysis, Prof. Sonam & Prof. Ankur are recommended for you.")
+    elif b=="2":
+        k=int(input('''Enter the manner of learning in which you are comfortable:- \nPress 1 for visualisation \nPress 2 for practical \nPress 3 for theoretical \nPress 4 for discussion based '''))
+        if k==1:
+            print("As per our analysis, Prof. animesh , prof. gauransh are recommended for you.")
+        elif k==2:
+            print("As per our analysis, Prof. Amulya & Prof. akmal are recommended for you.")
+        elif k==3:
+            print("As per our analysis, Prof. Ajay & Prof. subha are recommended for you.")
+        elif k==4:
+            print("As per our analysis, Prof. shantanu & Prof. sobha are recommended for you.") 
+
+    elif b=="3":
+        k=int(input('''Enter the manner of learning in which you are comfortable:- \nPress 1 for visualisation \nPress 2 for practical \nPress 3 for theoretical \nPress 4 for discussion based '''))
+        if k==1:
+            print("As per our analysis, Prof. akhil & Prof. asish are recommended for you.")
+        elif k==2:
+            print("As per our analysis, Prof. nikhil & Prof. shanti are recommended for you.")
+        elif k==3:
+            print("As per our analysis, Prof. shahrukh & Prof. subha are recommended for you.")
+        elif k==4:
+            print("As per our analysis, Prof. akshay & Prof. salman are recommended for you.") 
